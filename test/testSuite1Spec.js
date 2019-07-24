7137d7a3d8afd4e60c49947c09a4c2625764134f
Ad1B3nAr0ya <adi@cloudinary.com>
2019-07-24 11:53:42 +0300

    fixes

--- test/testSuite1Spec.js
+++ test/testSuite1Spec.js
@@ -1,15 +1,15 @@
-const describe = require("mocha");
-const AuthenticationService = require("infra/AuthenticationService.js");
+const expect = require('chai').expect
 
+const authenticationService_1 = require("../src/infra/AuthenticationService");
 
-const authenticationService = new AuthenticationService();
+const authService = authenticationService_1.authenticationService;
 
-describe("Suite Description - > Authentication Tests", () => {
-
+describe('Suite Description - > Authentication Tests', () => {
 
     it("ID_1- Login existing user",() => {
         // Test logic here...
-      const response =  authenticationService.login("borisflir+uatEU@gmail.com","Flir2018$", 600)
+
+      const response =  authService.login("borisflir+uatEU@gmail.com","Flir2018$", 600)
 
         // validation example...
         expect(response.email).equal("aaa@aaa.com");
@@ -18,7 +18,7 @@ describe("Suite Description - > Authentication Tests", () => {
 
     it("ID_2- Login non existing user",() => {
         // Test logic here...
-        authenticationService.login("lalala","blalbla", "1.1.2023")
+        authService.login("lalala","blalbla", "1.1.2023")
 
         // validation example...
         expect(authService.login("lalala","blalbla")).to.throw(Error)
